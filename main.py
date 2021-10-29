156490bd84369c94154494612925eabc789acaf8
Mark B <mark.behnke1998@gmail.com>
2021-10-28 20:45:15 -0700

    created protoype

--- main.py
+++ main.py
@@ -12,8 +12,17 @@ class main:
         url = template.format(position, location)
         return url
 
-    url = getURL('software developr', 'Canada')
+    url = getURL('software developer', 'Canada')
 
     response = requests.get(url)
 
     soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Get job titles
+    cards = soup.find_all(
+        'h2', class_='jobTitle jobTitle-color-purple jobTitle-newJob')
+
+    # Prototype the model with a single record:
+    card = cards[0].div
+
+    print(card['span'])
