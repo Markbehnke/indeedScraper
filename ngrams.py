9263f47c9864354f0dacf7822a9c9040b667ee14
Spyrunite <spyrunite13@gmail.com>
2021-12-11 12:56:26 -0800

    Merge branch 'main' of https://github.com/Markbehnke/indeedScraper into main

---
+++ ngrams.py
@@ -0,0 +1,32 @@
+from nltk import ngrams
+from nltk import tokenize
+import os
+import re
+
+allFiles = os.listdir('./processedLogs')
+gramDict = dict()
+dupeCheck = 0
+
+for i in range(len(allFiles)):
+    with open(f"./processedLogs/{allFiles[i]}", 'r', encoding='utf-8') as file:
+        data = file.read()
+        data = tokenize.sent_tokenize(data)
+
+        for n in range(1, 7):
+            for y in range(len(data)):
+                
+                data[y] = re.sub(r'[^A-Za-z0-9 ]+', '', data[y])
+                ngram = ngrams(data[y].split(), n)
+
+                for gram in ngram:
+                    if gram in gramDict:
+                        gramDict[gram]+=1
+                    else:
+                        gramDict[gram]=1
+            gramDict = sorted(gramDict.items(), key=lambda x: x[1], reverse=True)
+            with open(f"./ngram data/{n}gram for {allFiles[i]}", "w", encoding='utf-8') as writeFile:
+                for gram in gramDict:
+                    writeFile.write(str(gram))
+                    writeFile.write('\n')       
+
+            gramDict = dict()
