383b907af56724a924818dc86ce40399293e3605
Spyrunite <74026684+Spyrunite@users.noreply.github.com>
2021-12-12 09:53:34 -0800

    Update ngrams.py

--- ngrams.py
+++ ngrams.py
@@ -11,21 +11,25 @@ for i in range(len(allFiles)):
     with open(f"./processedLogs/{allFiles[i]}", 'r', encoding='utf-8') as file:
         data = file.read()
         lines = data.splitlines()
-        data = tokenize.sent_tokenize(data)
         lastLine = lines[-1]
         entryCount = int(lastLine)
+        data = tokenize.sent_tokenize(data)  
         for n in range(1, 7):
-            dupeArray = []
-            for y in range(len(data)):
-                
-                data[y] = re.sub(r'[^A-Za-z0-9 ]+', '', data[y])
-                ngram = ngrams(data[y].split(), n)
+            print("writing " + str(n) + "gram for file " + allFiles[i])
+            for line in lines:
+                data = tokenize.sent_tokenize(line)  
+                dupeArray = []
+                for y in range(len(data)):
+                    data[y] = re.sub(r'[^A-Za-z0-9 ]+', '', data[y])
+                    ngram = ngrams(data[y].split(), n)
 
-                for gram in ngram:
-                    if gram in gramDict:
-                        gramDict[gram]+=1
-                    else:
-                        gramDict[gram]=1
+                    for gram in ngram:
+                        if gram not in dupeArray:
+                            dupeArray.append(gram)
+                            if gram in gramDict:
+                                gramDict[gram]+=1
+                            else:
+                                gramDict[gram]=1
             gramDict = sorted(gramDict.items(), key=lambda x: x[1], reverse=True)
             with open(f"./ngram data/{n}gram for {allFiles[i]}", "w", encoding='utf-8') as writeFile:
                 for gram in gramDict:
